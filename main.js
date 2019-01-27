9c9837425146270dab1f52923e4cb5b972ba9abd
Peter Hoddie <peter@moddable.tech>
2019-01-26 17:45:03 -0800

    erase flash for full size of mod before installing; clean-up

--- main.js
+++ main.js
@@ -19,41 +19,48 @@ import {Server} from "http";
 
 function restart() @ "xs_restart";		// N.B. restart does not occur immediately. this function returns.
 
-
 /*
-	build, install, and launch host app:
+	build, install, and launch runmood (host) app:
 		cd $MODDAB:E/examples/experimental/runmod
-		mcconfig -d -m -p esp ssid=“579 9th - slow” password=kinomakinoma
+		mcconfig -d -m -p esp ssid=“MY_WIFI” password="MY_PASSWORD"
 
-	build and install helloworld or httpget mod
+	 build and install helloworld or httpget mod:
 		cd $MODDABLE/examples/experimental/runmod/mods/helloworld
 		xsc ./mod.js -d -e -o ./build
 		xsl -a -b ./build -o ./build -r mod ./build/mod.xsb
 		curl -T ./build/mod.xsa http://runmod.local/mod/install
-		curl http://runmod.local/mod/uninstall
 
-	build and install ping mod
+	 build and install ping mod:
 		cd $MODDABLE/examples/experimental/runmod/mods/ping
 		xsc ./mod.js -d -e -o ./build
 		xsc $MODDABLE/modules/network/ping/ping.js -d -e -o ./build
 		xsl -a -b ./build -o ./build -r mod ./build/mod.xsb ./build/ping.xsb
-		curl http://runmod.local/mod/uninstall
+ 		curl -T ./build/mod.xsa http://runmod.local/mod/install
+
+ 	uninstall mod
+ 		curl http://runmod.local/mod/uninstall
  */
 
 class ModServer extends Server {
 	callback(message, value, etc) {
 		switch (message) {
 			case 2:								// request status received
-				if (value === "/mod/install") {
+				if (value === "/mod/install")
 					this.flash = new Flash("xs_stage");
-					this.position = 0;
-					this.flash.erase(0);		//@@
-				}
 				else if (value === "/mod/uninstall") {
 					const flash = new Flash("xs_stage");
-					flash.write(0, 16, new ArrayBuffer(16));
-					trace("uninstalled mod");
+					flash.write(0, 16, new ArrayBuffer(16));		// overwrite XS Archive signature
+					trace("uninstalled mod\n");
 					this.server.restart();
+				}
+				break;
+
+			case 3:
+				if (this.flash && ("content-length" === value)) {	// must receive content-length for mod to be written to flash
+					const length = parseInt(etc);
+					for (let sector = 0; sector * this.flash.blockSize < length; sector += 1)
+						this.flash.erase(sector);
+					this.position = 0;
 				}
 				break;
 
@@ -61,7 +68,7 @@ class ModServer extends Server {
 				return true;					// provide request body in fragments
 
 			case 5:								// request body fragment
-				if (this.flash) {
+				if (undefined !== this.position) {
 					let buffer = this.read(ArrayBuffer);
 					trace(`received ${buffer.byteLength} bytes of mod\n`)
 					this.flash.write(this.position, buffer.byteLength, buffer);
@@ -70,11 +77,14 @@ class ModServer extends Server {
 				break;
 
 			case 6:								// request body received
-				if (this.flash) {
+				if (undefined !== this.position) {
 					trace("installed mod\n");
 					this.server.restart();
 				}
 				break;
+
+			case 8:
+				return {body: "done\n"};
 		}
 	}
 	restart() {
